94f9fb54612daa52183852909b5b72754a5af8ed
Xavier K <xavierkhew00@gmail.com>
2021-08-13 15:36:06 +0800

    add auth0 stuff in

---
+++ pages/api/auth/[...auth0].js
@@ -0,0 +1,6 @@
+import { handleAuth } from '@auth0/nextjs-auth0';
+
+export default handleAuth();
+
+// you can even put more configuration in this handleAuth()
+// https://auth0.github.io/nextjs-auth0/modules/config.html#2-create-your-own-instance-using-initauth0
